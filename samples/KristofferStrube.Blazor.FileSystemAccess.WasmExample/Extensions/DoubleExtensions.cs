34541118debce0fc7cbdc1d45cd3b51c30df2c01
Kristoffer Strube <kristoffer.strube@gmail.com>
2022-03-09 02:52:57 +0100

    Tree map Chart

---
+++ samples/KristofferStrube.Blazor.FileSystemAccess.WasmExample/Extensions/DoubleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+namespace KristofferStrube.Blazor.FileSystemAccess.WasmExample
+{
+    public static class DoubleExtensions
+    {
+        public static string AsString(this double d)
+        {
+            return d.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
