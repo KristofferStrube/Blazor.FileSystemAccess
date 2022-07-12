5a010800f72caa80c62c08580028c0bb953cbe8a
KristofferStrube <KristofferStrube@users.noreply.github.com>
2022-07-12 21:13:55 +0000

    deploy: 90efa1e275d801b38b310b17e1e093567dbbe890

---
+++ _content/KristofferStrube.Blazor.FileSystemAccess/KristofferStrube.Blazor.FileSystemAccess.js
@@ -0,0 +1,16 @@
+export function size(array) { return array.length; }
+
+export function getAttribute(object, attribute) { return object[attribute]; }
+
+export async function arrayFrom(values) {
+    var res = []
+    for await (let value of values) {
+        res.push(value);
+    }
+    return res;
+}
+
+export function WriteBlobWriteParams(fileSystemWritableFileStream, writeParams, blob) {
+    writeParams.data = blob;
+    fileSystemWritableFileStream.write(writeParams);
+}
