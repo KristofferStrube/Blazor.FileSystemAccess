2e94ceac3aa1df8d1a8dc682019c32c38936321f
KristofferStrube <KristofferStrube@users.noreply.github.com>
2025-04-08 11:37:53 +0000

    deploy: 9b8793dd8df063325491d3fd5ec641db90d9cfb6

---
+++ _content/KristofferStrube.Blazor.WebIDL/KristofferStrube.Blazor.WebIDL.js
@@ -0,0 +1,22 @@
+export function getAttribute(object, attribute) { return object[attribute]; }
+
+export function forEachWithNoArguments(jSReference, callbackObjRef) {
+    jSReference.forEach(() => callbackObjRef.invokeMethodAsync('InvokeCallback'))
+}
+
+export function forEachWithOneArgument(jSReference, callbackObjRef) {
+    jSReference.forEach((value) => callbackObjRef.invokeMethodAsync('InvokeCallback', DotNet.createJSObjectReference(value)))
+}
+
+export function forEachWithTwoArguments(jSReference, callbackObjRef) {
+    jSReference.forEach((value, key) => callbackObjRef.invokeMethodAsync('InvokeCallback', DotNet.createJSObjectReference(value, key)))
+}
+
+// https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
+export function valuePropertiesType(obj) {
+    return ({}).toString.call(obj.value).match(/\s([a-z|A-Z]+)/)[1].toLowerCase();
+}
+
+export function valuePropertiesValue(obj) {
+    return obj.value;
+}
