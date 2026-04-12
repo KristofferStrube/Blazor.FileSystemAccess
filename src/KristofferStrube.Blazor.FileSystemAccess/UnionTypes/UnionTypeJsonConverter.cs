83718f5eeb71bb3e1f0a0dc2ea77fc70c6c4973b
KristofferStrube <kristoffer.strube@gmail.com>
2026-04-12 17:19:49 +0200

    Updated exception message to fit this library.

--- src/KristofferStrube.Blazor.FileSystemAccess/UnionTypes/UnionTypeJsonConverter.cs
+++ src/KristofferStrube.Blazor.FileSystemAccess/UnionTypes/UnionTypeJsonConverter.cs
@@ -8,7 +8,7 @@ internal class UnionTypeJsonConverter<T> : JsonConverter<T> where T : UnionType
 {
     public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        throw new InvalidOperationException("Can't deserialize UnionTypes from the Blazor.MediaCaptureStreams library.");
+        throw new InvalidOperationException("Can't deserialize UnionTypes from the Blazor.FileSystemAccess library.");
     }
 
     public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
