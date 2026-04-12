f7e6cc5e04a4e213b80411ef71686e53a670478d
KristofferStrube <kristoffer.strube@gmail.com>
2026-04-12 17:10:59 +0200

    Updated summaries in `PermissionState` to link to values instead of using strings.

--- src/KristofferStrube.Blazor.FileSystemAccess/Enums/PermissionState.cs
+++ src/KristofferStrube.Blazor.FileSystemAccess/Enums/PermissionState.cs
@@ -23,8 +23,8 @@ public enum PermissionState
     Denied,
 
     /// <summary>
-    /// The user has not given express permission to use the feature (i.e., it's the same as "denied").
-    /// It also means that if a caller attempts to use the feature, the user agent will either be prompting the user for permission or access to the feature will be "denied".
+    /// The user has not given express permission to use the feature (i.e., it's the same as <see cref="Denied"/>).
+    /// It also means that if a caller attempts to use the feature, the user agent will either be prompting the user for permission or access to the feature will be <see cref="Denied"/>.
     /// </summary>
     Prompt,
 }
